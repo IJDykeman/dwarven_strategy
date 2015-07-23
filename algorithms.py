d53657d8d3bd11709d12d32108e4c19f05127a98
ijdykeman <ijdykeman@gmail.com>
2015-07-22 21:59:42 -0700

    first commit - basic map with objects.

---
+++ algorithms.py
@@ -0,0 +1,60 @@
+###   Algorithms   ###
+
+import heapq
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+    
+    def empty(self):
+        return len(self.elements) == 0
+    
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+    
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+def heuristic(a, b):
+    """
+    Manhattan distance from a to b.
+    a and b are (x,y) pairs.
+    """
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def move_cost(a,b):
+    return 1
+
+def get_path(start, goal):
+    """
+    Returns list of (x,y) which is the shortest path between start and goal.
+    Goal and start are (x,y)
+    """
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+    
+    while not frontier.empty():
+        current = frontier.get()
+        
+        if current == goal:
+            break
+        
+        for next in passable_from(current):
+            new_cost = cost_so_far[current] + move_cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+    if not goal in came_from:
+        return None
+    path = [goal]
+    while path[0] in came_from:
+        path.insert(0,came_from[path[0]])
+    return path[1:]
---
+++ thing.py
@@ -0,0 +1,111 @@
+import heapq
+from rules import *
+from algorithms import *
+
+###   Mutable World State ###
+
+state = []
+
+###   Querying Mutable World State   ###
+
+def select(args, obj_set = state):
+    result = []
+    for item in obj_set:
+        if all([key in item and item[key] == val for key, val in args]):
+            result.append(item)
+    return result
+
+def is_loc_passable(loc, obj_set=state):
+    return all([not is_of_kind(obj, 'impassable') 
+                for obj in select([(Keys.loc,loc)])])
+
+def passable_from(p, obj_set=state):
+    result = []
+    for loc in get_adjacent_tiles(p):
+        if is_loc_passable(loc, obj_set = obj_set):
+            result.append(loc)
+    return result
+
+###   Mutating World State   ###
+
+def add_obj_to_state(properties, obj_set=state):
+    if not Keys.inventory in properties:
+        properties[Keys.inventory] = []
+    assert Keys.kind in properties
+    properties[Keys.weight] = get_weight(properties[Keys.kind])
+    kind = properties[Keys.kind]
+    obj_set.append(properties)
+
+def add_object_at_all(kind_to_add, criteria):
+    places_to_add = select(criteria)
+    for obj in places_to_add:
+        loc = obj[Keys.loc]
+        add_obj_to_state({
+            Keys.kind: kind_to_add,
+            Keys.loc: loc
+            })
+
+def create_simple_world():
+    tiles = [
+    ",,,,,,,,,,,,,,,",
+    ",,,,,,,,,,,,,,,",
+    ",,,,,,,,,,,,,,,",
+    ",,,,,,,,,,,,,,,",
+    ",,,,,,,,,,,,,,,",
+    ",,,,,,,,,,,,,,,",
+    ",,,,,,,,,,,,,,,",
+    ",,,,,,,,,......",
+    ",,,,,,,,,......",
+    ",,,,,,,,,......",
+    ",,,,,,,,,......",
+    ",,,,,,,.,......",
+    ",,,,,,,.,......",
+    ",,,,,,.........",
+    ",,,,,,........."
+    ]
+    for x in range(WIDTH):
+        for y in range(WIDTH):
+            add_obj_to_state(
+                {
+                    Keys.kind: get_glyph_kind(tiles[y][x]),
+                    Keys.loc: (x,y)
+                })
+
+    add_obj_to_state({
+        Keys.name:'fili',
+        Keys.kind:'dwarf',
+        Keys.loc: (2,2),
+        Keys.inventory:  []
+        })
+    add_obj_to_state({
+        Keys.name:'ori',
+        Keys.kind:'dwarf',
+        Keys.loc: (2,6),
+        })
+    add_obj_to_state({
+        Keys.kind:'axe',
+        Keys.loc: (10,5),
+        })
+    add_obj_to_state({
+        Keys.kind:'tree',
+        Keys.loc: (7,8),
+        Keys.inventory:  [wood]
+        })
+
+### Display   ###
+
+def draw_world():
+    for x in range(WIDTH):
+        for y in range(WIDTH):
+            at_loc = select([(Keys.loc,(x,y))])
+            non_tiles = filter(is_not_tile, at_loc)
+            if non_tiles:
+                print get_glyph(non_tiles.pop()[Keys.kind]),
+            else:
+                print get_glyph(at_loc.pop()[Keys.kind]),
+        print''
+
+create_simple_world()
+add_object_at_all('cliff',[(Keys.kind,'stone_tile')])
+
+draw_world()
