6708931077792688de79977d80f2bd1bfa619c8a
ijdykeman <ijdykeman@gmail.com>
2015-07-24 23:46:15 -0700

    condition_met working and PEP8 compliant

--- libdwarf.py
+++ libdwarf.py
@@ -1,31 +1,31 @@
-from copy import deepcopy
 
 ###   Immutable state   ###
 
+
 class Keys:
-    loc  = 0
+    loc = 0
     name = 1
     kind = 3
     inventory = 4
     weight = 5
 
 ALL_KINDS = {
-    'wood' : {'weight': 'light'},
-    'axe' : {'weight': 'light'},
-    'dwarf' : {'weight': 'heavy'},
-    'cliff' : {'weight': 'massive'},
-    'tree' : {'weight': 'massive'}
+    'wood': {'weight': 'light'},
+    'axe': {'weight': 'light'},
+    'dwarf': {'weight': 'heavy'},
+    'cliff': {'weight': 'massive'},
+    'tree': {'weight': 'massive'}
 }
 
 GLYPH_MAP = {
-    'dwarf':'D',#u'\u263A',
-    'dirt_tile':',',
-    'stone_tile':'.',
-    'grass':'"',
-    'cliff':'#',
-    'axe':'a',
-    'tree':'T',
-    'wood':'w'
+    'dwarf': 'D',  # u'\u263A',
+    'dirt_tile': ',',
+    'stone_tile': '.',
+    'grass': '"',
+    'cliff': '#',
+    'axe': 'a',
+    'tree': 'T',
+    'wood': 'w'
 }
 
 SUBTYPES = {
@@ -35,28 +35,27 @@ SUBTYPES = {
 
 WIDTH = 15
 
+
 ###   Actions   ###
-
-# Actions are 3-tuples of (precontitions, action, postconditions)
-
-
-
 
 
 def create_actions_from_template():
+    # Actions are 3-tuples of (precontitions, action, postconditions)
     action_templates = [
-        ({'actor not_at any_type', 'actor has_path_to any_type', 'actor of_kind creature'}, 
-                'actor go_to any_type', 
-                {'actor at any_type'}),
-        
-        ({'actor at any_type', 'any_type is_of_weight light'}, 
-                'actor get any_type', 
-                {'actor has any_type'}),
-        
-        ({'actor at tree', 'actor has axe'}, 
-            'actor destroy tree', 
+        ({'actor not_at any_kind', 'actor has_path_to any_kind',
+          'actor of_kind creature'},
+            'actor go_to any_kind',
+         {'actor at any_kind', 'actor has any_kind'}),
+
+        ({'actor at any_kind', 'any_kind is_of_weight light'},
+         'actor get any_kind',
+         {'actor has any_kind'}),
+
+        ({'actor at tree', 'actor has axe'},
+            'actor destroy tree',
             {'actor at wood'})
     ]
+
     def action_includes_str(action, item):
         for contstraint in action[0].union(action[2]):
             if item in contstraint:
@@ -65,46 +64,57 @@ def create_actions_from_template():
             return True
         return False
 
-    def get_action_with_str_replaced(action, oldstr, newstr):
-        new_action = ({},"",{})
-        for contstraint in new_action[0]:
-            contstraint.replace(oldstr,newstr)
-        contstraint[1].replace(oldstr,newstr)
-        for contstraint in new_action[2]:
-            contstraint.replace(oldstr,newstr)
-        return new_action
+    def get_action_with_any_kind_replaced(action, oldstr, newstr):
+        """
+        gets list of new actions where each action is the old action with
+        any_kind replaced with each of the game's kinds
+        """
+        new_action = [set([]), "", set([])]
+        for contstraint in action[0]:
+            new_action[0].add(contstraint.replace(oldstr, newstr))
+        new_action[1] = action[1].replace(oldstr, newstr)
+        for contstraint in action[2]:
+            new_action[2].add(contstraint.replace(oldstr, newstr))
+        return tuple(new_action)
 
     result = []
     for action in action_templates:
-        if action_includes_str(action,'any_type'):
+        if action_includes_str(action, 'any_kind'):
             for kind in ALL_KINDS:
-                result.append(get_action_with_str_replaced(action,'any_type',kind))
+                result.append(get_action_with_any_kind_replaced(action,
+                              'any_kind', kind))
     return result
 
 
-ACTIONS = create_actions_from_template()
-
-for item in ACTIONS:
-    print item
 ###   Condition tests   ###
+
 
 def condition_met(condition, intermediary_state, actor=None, target=None):
     words = condition.split(' ')
     verb = words[1]
     if verb == 'at':
         if condition == 'actor at target':
+            assert actor != target
             return actor[Keys.loc] == target[Keys.loc]
         if 'actor at' in condition:
-            return len(select({(Keys.loc,actor[Keys.loc]), (Keys.kind, words[2])}))>0
-
+            matching_targets = select({(Keys.loc, actor[Keys.loc]),
+                                     (Keys.kind, words[2])})
+            # take only the targets that are not the actor.  Actor cannot be
+            # 'at' itself
+            matching_targets = filter(lambda x: x != actor, matching_targets)
+            return len(matching_targets) > 0
     if verb == 'not_at':
-        if condition == 'actor not_at target':
-            return actor[Keys.loc] != target[Keys.loc]
+        return not condition_met('actor at '+words[2], intermediary_state,
+                                 actor=actor, target=target)
 
     if verb == 'has_path_to':
         if condition == 'actor has_path_to target':
-            return get_path(actor[Keys.loc], target[Keys.loc]) != None
-
+            return get_path(actor[Keys.loc], target[Keys.loc]) is not None
+        else:  # concerned with a path to a kind
+            for item in select([(Keys.kind, words[2])]):
+                if get_path(item[Keys.loc], actor[Keys.loc]) is not None:
+                    return True
+            return False
     if verb == 'of_kind':
         if words[0] == 'actor':
             return is_of_kind(actor, words[2])
@@ -119,9 +129,9 @@ def condition_met(condition, intermediary_state, actor=None, target=None):
 
     if verb == 'has':
         if words[0] == 'actor':
-            return contains(actor, ('kind', words[2]))
+            return contains(actor, (Keys.kind, words[2]))
 
-    assert 1==0
+    assert 1 == 0
 
 
 ###   Querying Immutable Properties ###
@@ -136,6 +146,7 @@ def is_of_kind(obj, kind):
         return True
     return False
 
+
 def get_glyph(kind):
     """
     Returns the glyph for this kind if one is specified, else returns the first
@@ -144,6 +155,7 @@ def get_glyph(kind):
     if kind in GLYPH_MAP:
         return GLYPH_MAP[kind]
     return kind[0]
+
 
 def get_glyph_kind(glyph):
     for key in GLYPH_MAP:
@@ -154,20 +166,24 @@ def get_glyph_kind(glyph):
 def is_tile(obj):
     return 'tile' in obj[Keys.kind].split('_')
 
+
 def in_world(loc):
-    return 0<=loc[0]<WIDTH and 0<=loc[1]<WIDTH
+    return 0 <= loc[0] < WIDTH and 0 <= loc[1] < WIDTH
+
 
 def is_not_tile(obj):
     return not is_tile(obj)
+
 
 def get_adjacent_tiles(p):
     x = p[0]
     y = p[1]
     result = []
-    for loc in [(x+1,y),(x,y+1),(x-1,y),(x,y-1)]:
+    for loc in [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]:
         if in_world(loc):
             result.append(loc)
     return result
+
 
 def get_weight(kind):
     if kind in ALL_KINDS:
@@ -181,46 +197,52 @@ state = []
 
 ###   Querying Mutable World State   ###
 
-def select(args, obj_set = state):
+
+def select(args, obj_set=state):
     """
     returns all items in obj_set (state by default) that meet the contstraints
     set by the set of tuples {(attribute_key, desired_value)}
     """
+    #print args
+    #print obj_set
     result = []
     for item in obj_set:
         item_meets_criteria = True
         for key, val in args:
             if key == Keys.kind:
-                item_meets_criteria &= is_of_kind(item,val)
+                item_meets_criteria &= is_of_kind(item, val)
             else:
                 item_meets_criteria &= key in item and item[key] == val
             if not item_meets_criteria:
                 break
         if item_meets_criteria:
             result.append(item)
-            
     return result
 
+
 def is_loc_passable(loc, obj_set=state):
-    return all([not is_of_kind(obj, 'impassable') 
-                for obj in select([(Keys.loc,loc)])])
+    return all([not is_of_kind(obj, 'impassable')
+                for obj in select([(Keys.loc, loc)])])
+
 
 def passable_from(p, obj_set=state):
     result = []
     for loc in get_adjacent_tiles(p):
-        if is_loc_passable(loc, obj_set = obj_set):
+        if is_loc_passable(loc, obj_set=obj_set):
             result.append(loc)
     return result
 
 ###   Mutating World State   ###
+
 
 def add_obj_to_state(properties, obj_set=state):
     if not Keys.inventory in properties:
         properties[Keys.inventory] = []
     assert Keys.kind in properties
     properties[Keys.weight] = get_weight(properties[Keys.kind])
-    kind = properties[Keys.kind]
+    #kind = properties[Keys.kind]
     obj_set.append(properties)
+
 
 def add_object_at_all(kind_to_add, criteria):
     places_to_add = select(criteria)
@@ -229,32 +251,34 @@ def add_object_at_all(kind_to_add, criteria):
         add_obj_to_state({
             Keys.kind: kind_to_add,
             Keys.loc: loc
-            })
+        })
+
 
 def contains(subject, contstraints):
     """
     returns true if any item in subject's inventory matches contstraints
     """
-    return len(select(contstraints, subject[Keys.inventory])) > 0
+    return len(select([contstraints], obj_set=subject[Keys.inventory])) > 0
 
 ###   Algorithms   ###
 
 import heapq
 
 
-
 class PriorityQueue:
+
     def __init__(self):
         self.elements = []
-    
+
     def empty(self):
         return len(self.elements) == 0
-    
+
     def put(self, item, priority):
         heapq.heappush(self.elements, (priority, item))
-    
+
     def get(self):
         return heapq.heappop(self.elements)[1]
+
 
 def heuristic(a, b):
     """
@@ -265,8 +289,10 @@ def heuristic(a, b):
     (x2, y2) = b
     return abs(x1 - x2) + abs(y1 - y2)
 
-def move_cost(a,b):
+
+def move_cost(a, b):
     return 1
+
 
 def get_path(start, goal):
     """
@@ -279,13 +305,10 @@ def get_path(start, goal):
     cost_so_far = {}
     came_from[start] = None
     cost_so_far[start] = 0
-    
     while not frontier.empty():
         current = frontier.get()
-        
         if current == goal:
             break
-        
         for next in passable_from(current):
             new_cost = cost_so_far[current] + move_cost(current, next)
             if next not in cost_so_far or new_cost < cost_so_far[next]:
@@ -297,5 +320,5 @@ def get_path(start, goal):
         return None
     path = [goal]
     while path[0] in came_from:
-        path.insert(0,came_from[path[0]])
+        path.insert(0, came_from[path[0]])
     return path[1:]
--- thing.py
+++ thing.py
@@ -1,56 +1,54 @@
-import heapq
-from libdwarf import * 
-
+from libdwarf import *
 
 
 def create_simple_world():
     tiles = [
-    ",,,,,,,,,,,,,,,",
-    ",,,,,,,,,,,,,,,",
-    ",,,,,,,,,,,,,,,",
-    ",,,,,,,,,,,,,,,",
-    ",,,,,,,,,,,,,,,",
-    ",,,,,,,,,,,,,,,",
-    ",,,,,,,,,,,,,,,",
-    ",,,,,,,,,......",
-    ",,,,,,,,,......",
-    ",,,,,,,,,......",
-    ",,,,,,,,,......",
-    ",,,,,,,.,......",
-    ",,,,,,,.,......",
-    ",,,,,,.........",
-    ",,,,,,........."
+        ",,,,,,,,,,,,,,,",
+        ",,,,,,,,,,,,,,,",
+        ",,,,,,,,,,,,,,,",
+        ",,,,,,,,,,,,,,,",
+        ",,,,,,,,,,,,,,,",
+        ",,,,,,,,,,,,,,,",
+        ",,,,,,,,,,,,,,,",
+        ",,,,,,,,,......",
+        ",,,,,,,,,......",
+        ",,,,,,,,,......",
+        ",,,,,,,,,......",
+        ",,,,,,,.,......",
+        ",,,,,,,.,......",
+        ",,,,,,.........",
+        ",,,,,,........."
     ]
     for x in range(WIDTH):
         for y in range(WIDTH):
             add_obj_to_state(
                 {
                     Keys.kind: get_glyph_kind(tiles[y][x]),
-                    Keys.loc: (x,y)
+                    Keys.loc: (x, y)
                 })
 
-
     add_obj_to_state({
-        Keys.name:'ori',
-        Keys.kind:'dwarf',
-        Keys.loc: (2,6),
-        })
+        Keys.name: 'ori',
+        Keys.kind: 'dwarf',
+        Keys.loc: (2, 6),
+    })
     add_obj_to_state({
-        Keys.kind:'axe',
-        Keys.loc: (10,5),
-        })
+        Keys.kind: 'axe',
+        Keys.loc: (10, 5),
+    })
     add_obj_to_state({
-        Keys.kind:'tree',
-        Keys.loc: (7,8),
+        Keys.kind: 'tree',
+        Keys.loc: (7, 8),
         Keys.inventory:  ['wood']
-        })
+    })
 
 ### Display   ###
+
 
 def draw_world():
     for x in range(WIDTH):
         for y in range(WIDTH):
-            at_loc = select([(Keys.loc,(x,y))])
+            at_loc = select([(Keys.loc, (x, y))])
             non_tiles = filter(is_not_tile, at_loc)
             if non_tiles:
                 print get_glyph(non_tiles.pop()[Keys.kind]),
@@ -59,17 +57,31 @@ def draw_world():
         print''
 
 
-fili =  {
-        Keys.name:'fili',
-        Keys.kind:'dwarf',
-        Keys.loc: (2,2),
-        Keys.inventory:  []
-        }
+fili = {
+    Keys.name: 'fili',
+    Keys.kind: 'dwarf',
+    Keys.loc: (2, 2),
+    Keys.inventory:  []
+}
 
+add_obj_to_state({
+    Keys.kind: 'cliff',
+    Keys.loc: (2, 3),
+})
+add_obj_to_state({
+    Keys.kind: 'cliff',
+    Keys.loc: (2, 1),
+})
+add_obj_to_state({
+    Keys.kind: 'cliff',
+    Keys.loc: (1, 2),
+})
+add_obj_to_state({
+    Keys.kind: 'cliff',
+    Keys.loc: (3, 2),
+})
 add_obj_to_state(fili)
 create_simple_world()
-add_object_at_all('cliff',[(Keys.kind,'stone_tile')])
+add_object_at_all('cliff', [(Keys.kind, 'stone_tile')])
 
 draw_world()
-
-print condition_met('actor at tree', None, actor = fili)
